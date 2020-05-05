f55a52bccd1c669dd2bfea03a750807919b74405
Nazim Uddin <umnazim@gmail.com>
2020-05-05 01:47:33 -0700

    scrape news title, paragraph and featured image

--- app/scrape_mars.py
+++ app/scrape_mars.py
@@ -8,7 +8,7 @@ import re
 
 
 def mars_news(browser):
-    url = "https://mars.nasa.gov.news/"
+    url = "https://mars.nasa.gov/news/"
     browser.visit(url)
 
 # Get first list itemwait hold a second if not immediately present
@@ -18,16 +18,42 @@ def mars_news(browser):
     news_soup = BeautifulSoup(html, "html.parser")
     try:
         slide_elem = news_soup.select_one("ul.item_list li.slide")
-        new_title = slide_elem.find("div", class_="content_title").get_text()
+        news_title = slide_elem.find("div", class_="content_title").get_text()
         news_p = slide_elem.find(
             "div", class_="article_teaser_body").get_text()
     except AttributeError:
         return None, None
-    return
+    return news_title, news_p
 
 
 def featured_image(browser):
-    return
+    url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(url)
+    # Find and click the full image button
+    full_image_elem = browser.find_by_id("full_image")
+    full_image_elem.click()
+
+    # Find the info button and click that
+    browser.is_element_present_by_text("more info", wait_time=0.5)
+    more_info_elem = browser.links.find_by_partial_text('more info')
+    more_info_elem.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = BeautifulSoup(html, "html.parser")
+
+    # Find the relative image url
+    img = img_soup.select_one("figure.lede a img")
+
+    try:
+        img_url_rel = img.get("src")
+
+    except AttributeError:
+        return None
+    # use the base url to create an absolute url
+    img_url = f"https://www.jpl.nasa.gov{img_url_rel}"
+
+    return img_url
 
 
 def hemisphere(browser):
@@ -48,24 +74,30 @@ def mars_facts():
         df = pd.read_html("https://space-facts.com/mars/")[0]
     except BaseException:
         return None
-        df.columns = ["description", "value"]
-        df.set_index("description", inplace=True)
+    df.columns = ["description", "value"]
+    df.set_index("description", inplace=True)
+
     return df.to_html(classes="table table-striped")
 
 
-if __name__ == "__main__":
-    print(scrape_all())
-
-
 def scrape_all():
-    executable_path = {"executable_path": "chromedrive.exe"}
-    browser = Browser("chrome", executable_path="chromedriver", headless=False)
+    executable_path = {"executable_path": "chromedriver.exe"}
+    browser = Browser('chrome', **executable_path, headless=False)
 
     news_title, news_paragraph = mars_news(browser)
 
     data = {
+
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
         "facts": mars_facts(),
         "last_modified": dt.datetime.now()
     }
+    # browser.quit()
 
     return data
+
+
+if __name__ == "__main__":
+    print(scrape_all())
