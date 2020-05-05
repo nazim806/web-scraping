5ed2e2c27df8172002f944a192a49c41450e305d
Nazim Uddin <umnazim@gmail.com>
2020-05-04 23:29:25 -0700

    install packages, create functions in scrape_mars

--- app/scrape_mars.py
+++ app/scrape_mars.py
@@ -1,17 +1,28 @@
 
 from splinter import Browser
 from bs4 import BeautifulSoup
-import pandas as import pd
+import pandas as pd
 import datetime as dt
 import time
 import re
 
 
-def scrape_all():
-    return
-
-
 def mars_news(browser):
+    url = "https://mars.nasa.gov.news/"
+    browser.visit(url)
+
+# Get first list itemwait hold a second if not immediately present
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=0.5)
+
+    html = browser.html
+    news_soup = BeautifulSoup(html, "html.parser")
+    try:
+        slide_elem = news_soup.select_one("ul.item_list li.slide")
+        new_title = slide_elem.find("div", class_="content_title").get_text()
+        news_p = slide_elem.find(
+            "div", class_="article_teaser_body").get_text()
+    except AttributeError:
+        return None, None
     return
 
 
@@ -27,7 +38,8 @@ def twitter_weather(browser):
     return
 
 
-def scrape_hemispher():
+def scrape_hemispher(html_text):
+
     return
 
 
@@ -43,3 +55,17 @@ def mars_facts():
 
 if __name__ == "__main__":
     print(scrape_all())
+
+
+def scrape_all():
+    executable_path = {"executable_path": "chromedrive.exe"}
+    browser = Browser("chrome", executable_path="chromedriver", headless=False)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    data = {
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    return data
